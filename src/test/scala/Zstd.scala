0a8d762c3d6048b8981481e99db5691790bca01b
luben karavelov <luben@amazon.com>
2015-11-02 13:14:59 +0000

    remove unneeded scopes in tests

--- src/test/scala/Zstd.scala
+++ src/test/scala/Zstd.scala
@@ -12,43 +12,39 @@ class ZstdSpec extends FlatSpec with Checkers {
 
   "Zstd" should "should round-trip compression/decompression" in {
     check { input: Array[Byte] =>
-      {
-        val size        = input.length
-        val compressed  = Zstd.compress(input)
-        val decompressed= Zstd.decompress(compressed, size)
-        input.toSeq == decompressed.toSeq
-      }
+      val size        = input.length
+      val compressed  = Zstd.compress(input)
+      val decompressed= Zstd.decompress(compressed, size)
+      input.toSeq == decompressed.toSeq
     }
   }
 
  it should "should round-trip using streaming API" in {
     check { input: Array[Byte] =>
-      {
-        val size  = input.length
-        val os    = new ByteArrayOutputStream(Zstd.compressBound(size.toLong).toInt)
-        val zos   = new ZstdOutputStream(os)
-        var ptr   = 0
-        while (ptr < size - 1) {
-          val chunk = if (size - ptr > 128 * 1024) 128 * 1024 else size - ptr
-          zos.write(input, ptr, chunk)
-          ptr += chunk
-        }
-        zos.close
-        val compressed = os.toByteArray
-        // now decompress
-        val is    = new ByteArrayInputStream(compressed)
-        val zis   = new ZstdInputStream(is)
-        val output= Array.fill[Byte](size)(0)
-        ptr       = 0
-
-        while (ptr < size - 1) {
-          val chunk = if (size - ptr > 128 * 1024) 128 * 1024 else size - ptr
-          zis.read(output, ptr, chunk)
-          ptr += chunk
-        }
-        zis.close
-        input.toSeq == output.toSeq
+      val size  = input.length
+      val os    = new ByteArrayOutputStream(Zstd.compressBound(size.toLong).toInt)
+      val zos   = new ZstdOutputStream(os)
+      var ptr   = 0
+      while (ptr < size - 1) {
+        val chunk = if (size - ptr > 128 * 1024) 128 * 1024 else size - ptr
+        zos.write(input, ptr, chunk)
+        ptr += chunk
       }
+      zos.close
+      val compressed = os.toByteArray
+      // now decompress
+      val is    = new ByteArrayInputStream(compressed)
+      val zis   = new ZstdInputStream(is)
+      val output= Array.fill[Byte](size)(0)
+      ptr       = 0
+
+      while (ptr < size - 1) {
+        val chunk = if (size - ptr > 128 * 1024) 128 * 1024 else size - ptr
+        zis.read(output, ptr, chunk)
+        ptr += chunk
+      }
+      zis.close
+      input.toSeq == output.toSeq
     }
   }
 }
