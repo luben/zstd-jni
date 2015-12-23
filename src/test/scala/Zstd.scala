d90e7c7d8430153f80a670f95d80d878c9e403a0
luben karavelov <luben@amazon.com>
2015-12-23 13:38:18 +0000

    Make the compatibility tests fail
    if generated output is not the expected one

--- src/test/scala/Zstd.scala
+++ src/test/scala/Zstd.scala
@@ -6,6 +6,7 @@ import org.scalacheck.Arbitrary._
 import org.scalacheck.Prop._
 import java.io._
 import scala.io._
+import scala.collection.mutable.WrappedArray
 
 class ZstdSpec extends FlatSpec with Checkers {
   implicit override val generatorDrivenConfig =
@@ -61,35 +62,49 @@ class ZstdSpec extends FlatSpec with Checkers {
     }
   }
 
-
   for (level <- List(1,3,6,9))
     "ZstdInputStream" should s"be able to consume files compressed by the zstd binary at level $level" in {
       val orig = new File("src/test/resources/xml")
       val file = new File(s"src/test/resources/xml-$level.zst")
       val fis  = new FileInputStream(file)
       val zis  = new ZstdInputStream(fis)
-      val buff = Array.fill[Byte](orig.length.toInt)(0)
-      zis.read(buff, 0, orig.length.toInt)
-      val original = Source.fromFile(orig)(Codec.ISO8859)
-      original.toSeq == buff.toSeq
+      assert(!zis.markSupported)
+      assert(zis.available == 0)
+      assert(zis.skip(0) == 0)
+      val length = orig.length.toInt
+      val buff = Array.fill[Byte](length)(0)
+      var pos  = 0;
+      while (pos < length) {
+        pos += zis.read(buff, pos, length - pos)
+      }
+
+      val original = Source.fromFile(orig)(Codec.ISO8859).map{char => char.toByte}.to[WrappedArray]
+      if(original != buff.toSeq)
+        sys.error(s"Failed")
     }
 
   for (level <- List(1,3,6,9))
     "ZstdOutputStream" should s"produce the same compressed file as zstd binary at level $level" in {
       val file = new File("src/test/resources/xml")
+      val length = file.length.toInt
       val fis  = new FileInputStream(file)
-      val buff = Array.fill[Byte](file.length.toInt)(0)
-      fis.read(buff, 0, file.length.toInt)
+      val buff = Array.fill[Byte](length)(0)
+      var pos  = 0
+      while( pos < length) {
+        pos += fis.read(buff, pos, length - pos)
+      }
 
       val os  = new ByteArrayOutputStream(Zstd.compressBound(file.length).toInt)
       val zos = new ZstdOutputStream(os, level)
       zos.write(buff)
+      zos.flush
       zos.close
 
-      val compressed = os.toByteArray
-      val zst = Source.fromFile(s"src/test/resources/xml-$level.zst")(Codec.ISO8859)
+      val compressed = os.toByteArray.toSeq
+      val zst = Source.fromFile(s"src/test/resources/xml-$level.zst")(Codec.ISO8859).map{char => char.toByte}.to[WrappedArray]
 
-      zst.toSeq == compressed.toSeq
+      if (zst != compressed)
+        sys.error(s"Failed")
     }
 
 }
