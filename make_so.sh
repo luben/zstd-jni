83fa9fe3331de81b35980c211f722a02febca079
luben karavelov <karavelov@gmail.com>
2020-10-23 23:34:57 +0100

    AIX: compile it with xlc
    When compiled with gcc it links to `libgcc` that is not commonly
    available on AIX installs.

--- make_so.sh
+++ make_so.sh
@@ -9,17 +9,31 @@ compile () {
     INSTALL=target/classes/$OS/$ARCH
     rsync -r --delete jni $HOST:
     rsync -r --delete src/main/native $HOST:
-    ssh $HOST 'export PATH=$HOME/bin:$PATH; '$CC' -shared -DDYNAMIC_BMI2=0 -fPIC -O3 -DZSTD_LEGACY_SUPPORT=4 -DZSTD_MULTITHREAD=1 -I/usr/include -I./jni -I./native -I./native/common -I./native/legacy -std=c99 -lpthread -o libzstd-jni.so native/*.c native/legacy/*.c native/common/*.c native/compress/*.c native/decompress/*.c native/dictBuilder/*.c'
+    ssh $HOST 'export PATH=$HOME/bin:$PATH; '$CC' -shared -flto -DDYNAMIC_BMI2=0 -fPIC -O3 -DZSTD_LEGACY_SUPPORT=4 -DZSTD_MULTITHREAD=1 -I/usr/include -I./jni -I./native -I./native/common -I./native/legacy -std=c99 -lpthread -o libzstd-jni.so native/*.c native/legacy/*.c native/common/*.c native/compress/*.c native/decompress/*.c native/dictBuilder/*.c'
     mkdir -p $INSTALL
     scp $HOST:libzstd-jni.so $INSTALL
 }
 
-compile amd64   linux "gcc -flto"
-compile i386    linux "gcc -march=i586 -flto"
-compile ppc64   linux "gcc -flto"
-compile ppc64le linux "gcc -flto"
-compile aarch64 linux "gcc -flto"
-compile mips64  linux "gcc -flto"
-compile amd64   freebsd "cc -flto"
-compile i386    freebsd "cc -m32 -march=i486 -mfancy-math-387 -flto"
-compile ppc64   aix
+compile_ppc64_aix () {
+    ARCH=ppc64
+    OS=aix
+    CC=xlc
+    HOST="gcc-$ARCH-$OS"
+    echo "Compiling for $ARCH/$OS on $HOST"
+    INSTALL=target/classes/$OS/$ARCH
+    rsync -r --delete jni $HOST:
+    rsync -r --delete src/main/native $HOST:
+    ssh $HOST 'export PATH=$HOME/bin:$PATH; '$CC' -bshared -brtl -G -lpthread -DDYNAMIC_BMI2=0 -O3 -DZSTD_LEGACY_SUPPORT=4 -DZSTD_MULTITHREAD=1 -I/usr/include -I./jni -I./native -I./native/common -I./native/legacy -o libzstd-jni.so native/*.c native/legacy/*.c native/common/*.c native/compress/*.c native/decompress/*.c native/dictBuilder/*.c'
+    mkdir -p $INSTALL
+    scp $HOST:libzstd-jni.so $INSTALL
+}
+
+compile amd64   linux
+compile i386    linux "gcc -march=i586"
+compile ppc64   linux
+compile ppc64le linux
+compile aarch64 linux
+compile mips64  linux
+compile amd64   freebsd "cc"
+compile i386    freebsd "cc -m32 -march=i486 -mfancy-math-387"
+compile_ppc64_aix
