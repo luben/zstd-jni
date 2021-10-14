f3abbcdab000a6ece760593673ad28d2f844c88a
Jonathan Albrecht <jonathan.albrecht@ibm.com>
2021-10-14 15:37:20 -0400

    Add a tag to some tests that are affected by https://github.com/facebook/zstd/issues/2736 on big endian platforms so they can be skipped for now.

--- .github/actions/qemu-cross/build.sh
+++ .github/actions/qemu-cross/build.sh
@@ -4,4 +4,11 @@ set -e
 
 apt-get update
 apt-get install -y gcc
-./sbt -v test package
+
+SBT_CMD="test"
+# On big endian platforms, exclude some tests that are affected by
+# https://github.com/facebook/zstd/issues/2736 in zstd 1.5.0.
+if [[ $(uname -m) == "s390x" ]]; then
+  SBT_CMD="testOnly * -- -l ZstdIssue2736"
+fi
+./sbt -v "${SBT_CMD}"
