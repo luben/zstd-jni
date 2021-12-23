3c34b68763a00957f848c4ac249fc3e3b8fa5612
luben karavelov <karavelov@gmail.com>
2021-12-23 17:27:07 +0000

    Fixes for the build to inclide the new ASM source

--- make_so.sh
+++ make_so.sh
@@ -13,22 +13,7 @@ compile () {
     INSTALL=target/classes/$OS/$ARCH
     rsync -r --delete jni $HOST:
     rsync -r --delete src/main/native $HOST:
-    ssh $HOST 'export PATH=$HOME/bin:$PATH; '$CC' -shared -flto -DDYNAMIC_BMI2=0 -fPIC -O3 -DZSTD_LEGACY_SUPPORT=4 -DZSTD_MULTITHREAD=1 -I/usr/include -I./jni -I./native -I./native/common -I./native/legacy -std=c99 -lpthread -o libzstd-jni-'$VERSION'.so native/*.c native/legacy/*.c native/common/*.c native/compress/*.c native/decompress/*.c native/dictBuilder/*.c'
-    mkdir -p $INSTALL
-    scp $HOST:libzstd-jni-$VERSION.so $INSTALL
-}
-
-compile_ppc64_aix () {
-    ARCH=ppc64
-    OS=aix
-    CC=xlc_r
-    HOST="gcc-$ARCH-$OS"
-    echo "Compiling for $ARCH/$OS on $HOST"
-    INSTALL=target/classes/$OS/$ARCH
-    ssh $HOST rm -rf jni native
-    scp -r jni $HOST:
-    scp -r src/main/native $HOST:
-    ssh $HOST 'export PATH=$HOME/bin:$PATH; '$CC' -q64 -bshared -brtl -G -DDYNAMIC_BMI2=0 -O3 -DZSTD_LEGACY_SUPPORT=4 -DZSTD_MULTITHREAD=1 -I/usr/include -I./jni -I./native -I./native/common -I./native/legacy -o libzstd-jni-'$VERSION'.so native/*.c native/legacy/*.c native/common/*.c native/compress/*.c native/decompress/*.c native/dictBuilder/*.c'
+    ssh $HOST 'export PATH=$HOME/bin:$PATH; '$CC' -shared -flto -fPIC -O3 -DZSTD_LEGACY_SUPPORT=4 -DZSTD_MULTITHREAD=1 -I/usr/include -I./jni -I./native -I./native/common -I./native/legacy -std=c99 -lpthread -o libzstd-jni-'$VERSION'.so native/*.c native/legacy/*.c native/common/*.c native/compress/*.c native/decompress/*.[cS] native/dictBuilder/*.c'
     mkdir -p $INSTALL
     scp $HOST:libzstd-jni-$VERSION.so $INSTALL
 }
@@ -41,4 +26,3 @@ compile aarch64 linux
 compile mips64  linux
 compile amd64   freebsd "cc"
 compile i386    freebsd "cc -m32 -march=i486 -mfancy-math-387"
-# compile_ppc64_aix
--- make_so_cross.sh
+++ make_so_cross.sh
@@ -17,7 +17,7 @@ compile() {
     rsync -r --delete src/main/native $BUILD_DIR
 
     pushd $BUILD_DIR
-    $CC -shared -static-libgcc -DDYNAMIC_BMI2=0 -fPIC -O3 -flto -DZSTD_LEGACY_SUPPORT=4 -DZSTD_MULTITHREAD=1 -I/usr/include -I./jni -I./native -I./native/common -I./native/legacy -std=c99 -lpthread -o libzstd-jni-$VERSION.so native/*.c native/legacy/*.c native/common/*.c native/compress/*.c native/decompress/*.c native/dictBuilder/*.c
+    $CC -shared -static-libgcc -fPIC -O3 -flto -DZSTD_LEGACY_SUPPORT=4 -DZSTD_MULTITHREAD=1 -I/usr/include -I./jni -I./native -I./native/common -I./native/legacy -std=c99 -lpthread -o libzstd-jni-$VERSION.so native/*.c native/legacy/*.c native/common/*.c native/compress/*.c native/decompress/*.c native/dictBuilder/*.c
     popd
 
     mkdir -p $INSTALL
