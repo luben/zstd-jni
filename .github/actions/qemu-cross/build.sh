1e7ea4d4ec144ad2cd52a3f26ab02eb9938c9943
Jonathan Albrecht <jonathan.albrecht@ibm.com>
2022-01-07 13:14:34 -0500

    Remove the tag on some tests that was added for https://github.com/facebook/zstd/issues/2736 on big endian platforms now that the issue is resolved in zstd 1.5.1.

--- .github/actions/qemu-cross/build.sh
+++ .github/actions/qemu-cross/build.sh
@@ -6,9 +6,4 @@ apt-get update
 apt-get install -y gcc
 
 SBT_CMD="test"
-# On big endian platforms, exclude some tests that are affected by
-# https://github.com/facebook/zstd/issues/2736 in zstd 1.5.0.
-if [[ $(uname -m) == "s390x" ]]; then
-  SBT_CMD="testOnly * -- -l ZstdIssue2736"
-fi
 ./sbt -v "${SBT_CMD}"
