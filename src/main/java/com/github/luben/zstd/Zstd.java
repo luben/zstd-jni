97550e35610cd36e2ad510e20e503c0f997c1a3a
luben karavelov <karavelov@gmail.com>
2022-02-09 01:19:47 +0000

    Remove frameHeaderSize(Min|Max)
    The underlying native methods are long gone. There is no anymore one
    MIN number and the zstd headers define the new macros as "experimental".
    
    Addresses #204

--- src/main/java/com/github/luben/zstd/Zstd.java
+++ src/main/java/com/github/luben/zstd/Zstd.java
@@ -754,8 +754,6 @@ public class Zstd {
     public static native int searchLogMax();
     public static native int searchLengthMin();
     public static native int searchLengthMax();
-    public static native int frameHeaderSizeMin();
-    public static native int frameHeaderSizeMax();
     public static native int blockSizeMax();
     public static native int defaultCompressionLevel();
     /* Min/max compression levels */
