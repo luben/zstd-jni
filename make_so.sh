0afb51a8420183cfb894063ea06cb08b91f63ce2
Luben Karavelov <karavelov@gmail.com>
2026-05-01 02:46:08 +0100

    Use gcc on AIX

--- make_so.sh
+++ make_so.sh
@@ -25,27 +25,28 @@ compile () {
     scp $HOST:libzstd-jni-$VERSION.so $INSTALL
 }
 
-compile_ppc64_aix () {
-    ARCH=ppc64
-    OS=aix
-    CC=xlc_r
+compile_aix() {
+    ARCH=$1
+    OS=$2
+    CC=${3:-"gcc -static-libgcc"}
     HOST="gcc-$ARCH-$OS"
     echo "Compiling for $ARCH/$OS on $HOST"
     INSTALL=target/classes/$OS/$ARCH
     ssh $HOST rm -rf jni native
     scp -r jni $HOST:
     scp -r src/main/native $HOST:
-    ssh $HOST 'export PATH=$HOME/bin:$PATH; '$CC' -q64 -bshared -brtl -G -DDYNAMIC_BMI2=0 -O3 -DZSTD_LEGACY_SUPPORT=4 -DZSTD_MULTITHREAD=1 -I/usr/include -I./jni -I./native -I./native/common -I./native/legacy -o libzstd-jni-'$VERSION'.so native/*.c native/legacy/*.c native/common/*.c native/compress/*.c native/decompress/*.c native/dictBuilder/*.c'
+    ssh $HOST 'export PATH=$HOME/bin:/opt/freeware/bin:$PATH; '$CC' -shared -fPIC -O3 -DZSTD_LEGACY_SUPPORT=4 -DZSTD_MULTITHREAD=1 -I/usr/include -I./jni -I./native -I./native/common -I./native/legacy -std=c99 -lpthread -o libzstd-jni-'$VERSION'.so native/*.c native/legacy/*.c native/common/*.c native/compress/*.c native/decompress/*.[cS] native/dictBuilder/*.c'
     mkdir -p $INSTALL
     scp $HOST:libzstd-jni-$VERSION.so $INSTALL
 }
+
 
 compile amd64   linux
 compile ppc64   linux
 compile ppc64le linux
 compile aarch64 linux
-#compile mips64  linux
+compile mips64  linux
 compile loongarch64  linux
 compile amd64   freebsd "cc"
 compile i386    freebsd "cc -m32 -march=i486 -mfancy-math-387"
-compile_ppc64_aix
+compile_aix ppc64 aix
