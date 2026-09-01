f1726b77854ec9d35350eefcf291c744170ac933
michal.zyga <michal.zyga03@gmail.com>
2026-09-01 07:06:18 +0200

    adjust infrastructure to utilize Multi-Release Jar feature for supporting JNI and FFM at the same time

--- .github/actions/qemu-cross/build.sh
+++ .github/actions/qemu-cross/build.sh
@@ -9,5 +9,10 @@ pushd sbt-java-module-info
 ./sbt publishLocal
 popd
 
-SBT_CMD="test"
-./sbt -v "${SBT_CMD}"
+# The same suite against both implementations. -Dzstd.ffm=true makes sbt prepend
+# target/classes/META-INF/versions/22 to the test classpath, so the versioned
+# classes shadow their base counterparts. It has to be a second sbt process: the
+# property is read while build.sbt is evaluated, and a JNI library can only be
+# System.load-ed by one classloader per JVM.
+./sbt -v test
+./sbt -v -Dzstd.ffm=true test
